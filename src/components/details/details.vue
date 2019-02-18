96531efb2251ac87110d1c44949fbd3e56c0825c
fady73 <fadykhayrat@gmail.com>
2019-02-19 01:22:04 +0200

    add bio massage

--- src/components/details/details.vue
+++ src/components/details/details.vue
@@ -8,16 +8,39 @@
  </div>
 <div class="col-12">
    <p >label:{{ Detial_user.login }}</p>
-   </div>
 </div>
+
+<div class="col-12">
+    <hr>  
+ </div>
+<div class="col-12">
+   <p> bio:{{ this.results.bio }}</p>
+   </div>
+
+</div>
+
+
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Details',
   props: {
     Detial_user: String
-  }
+  },
+  data(){
+         return{ results: []}
+        },
+        mounted() {
+          axios.get('https://api.github.com/users/'+this.Detial_user.login).then(response => {
+            this.results = response.data
+            if(this.results.bio==null){
+            this.results.bio="No bio"
+            }
+          })
+        }
 
 }
 </script>
