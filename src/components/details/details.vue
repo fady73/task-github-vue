2d7ffa7c4b898eb14ff0f859d1132419185e404f
fady73 <fadykhayrat@gmail.com>
2019-02-18 21:13:20 +0200

    add some style for user details

--- src/components/details/details.vue
+++ src/components/details/details.vue
@@ -1,8 +1,11 @@
 <template>
 <div class="row">
 <div class="col-12">
-       <img :src="Detial_user.avatar_url" class="img-fluid">
+       <img :src="Detial_user.avatar_url" class="img-fluid img">
    </div>
+  <div class="col-12">
+    <hr>  
+ </div>
 <div class="col-12">
    <p >label:{{ Detial_user.login }}</p>
    </div>
@@ -19,3 +22,10 @@ export default {
 }
 </script>
 
+<style>
+
+.img {
+   width:300px;
+}
+
+</style>
